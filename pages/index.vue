79e351893a616925cc72e5dad2a6e3ac123c23a7
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2024-08-31 06:48:57 +0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-red-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel suscipit rem corporis porro at quisquam in,
+        consequuntur ipsum architecto placeat deleniti illo, perferendis cupiditate exercitationem quidem perspiciatis
+        alias dolores nesciunt!
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
