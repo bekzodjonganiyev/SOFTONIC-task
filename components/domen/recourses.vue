d678a3ec6f7517aaa1e92b74fc43a58d33cddf6b
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2024-09-04 00:38:59 +0500

    footer added and desktop variant complated

--- components/domen/recourses.vue
+++ components/domen/recourses.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <h2 class="text-5xl font-bold text-center text-[var(--text-primary)] mb-14">Resources to help you get the <br> most out of Neon</h2>
+    <div class="container">
+        <h2 class="text-5xl font-bold text-center text-[var(--text-primary)] mb-14">Resources to help you get the <br>most out of Neon</h2>
 
         <div class="flex items-center justify-center gap-5 mb-16">
             <ReusableChipButton class-name="flex items-center gap-2 hover:rotate-2 group">
@@ -22,14 +22,72 @@
             </ReusableChipButton>
         </div>
 
-        <div class="grid grid-cols-4 gap-6">
+        <div class="grid grid-cols-4 gap-6 mb-20">
+            <!-- <div v-for="(resoursce, id) in resources" :key="id" class="card">
+                <span :inner-h-t-m-l="resoursce.icon"></span>
+                <span>{{ resoursce.name }}</span>
+            </div> -->
+            <div class="card p-6 flex flex-col items-start">
+                <SvgTelegram class="-ml-4" />
+                <p class="text-xl text-[var(--text-primary)] font-medium">Neon Telegram</p>
+            </div>
 
+            <div class="card p-6 flex flex-col items-start">
+                <SvgDiscord class="-ml-4" />
+                <p class="text-xl text-[var(--text-primary)] font-medium">Neon Telegram</p>
+            </div>
+
+            <div class="card p-6 flex flex-col items-start">
+                <SvgDocs class="-ml-4" />
+                <p class="text-xl text-[var(--text-primary)] font-medium">Neon Telegram</p>
+            </div>
+
+            <div class="card p-6 flex flex-col items-start">
+                <SvgVideo class="-ml-4" />
+                <p class="text-xl text-[var(--text-primary)] font-medium">Neon Telegram</p>
+            </div>
+        </div>
+
+        <div class="flex items-center justify-between p-10 banner-bg">
+            <div>
+                <h3 class="text-4xl font-bold text-[var(--text-primary)] mb-1">Get started with Neon</h3>
+                <p class="text-[#BFDBFE]">It only takes a few minutes to get started with Neon. Understand your users, start free, today.</p>
+            </div>
+
+            <button class="btn btn-primary">Start Free Trial -></button>
         </div>
     </div>
 </template>
 
 <script setup>
+// import Discord from '../svg/discord.vue';
+// import Docs from '../svg/docs.vue';
+// import Telegram from '../svg/telegram.vue';
+// import Video from '../svg/video.vue';
 
+// const resources = [
+//     {
+//         icon: <Telegram />,
+//         name: "Neon Telegram"
+//     },
+//     {
+//         icon: <Discord />,
+//         name: "Neon Discord"
+//     },
+//     {
+//         icon: <Docs />,
+//         name: "Neon TelegraDocs"
+//     },
+//     {
+//         icon: <Video />,
+//         name: "Neon Tutorials"
+//     }
+// ]
 </script>
 
-<style scoped></style>
+<style scoped>
+.banner-bg {
+    border-radius: 4px;
+    background: linear-gradient(45deg, #2563EB 1.12%, #A855F7 99.43%);
+}
+</style>
--- components/footer.vue
+++ components/footer.vue
@@ -1,6 +1,51 @@
 <template>
-    <footer>
-        Footer
+    <footer class="container grid grid-cols-5 gap-20 py-12 text-[var(--text-secondary)]">
+        <div>
+            <SvgMainLogo class="mb-4" />
+            <div class="flex items-center gap-1">
+                <NuxtLink to="#">Terms</NuxtLink>
+                <span> · </span>
+                <NuxtLink to="#">Privacy Policy</NuxtLink>
+            </div>
+        </div>
+        
+        <div>
+            <ul class="space-y-2">
+                <p class="font-medium text-[var(--text-primary)]">PRODUCTS</p>
+                <li><NuxtLink to="#">Neon Manage</NuxtLink></li>
+                <li><NuxtLink to="#">Neon Analyse</NuxtLink></li>
+                <li><NuxtLink to="#">Neon Launch</NuxtLink></li>
+                <li><NuxtLink to="#">Experimentation</NuxtLink></li>
+            </ul>
+        </div>
+        <div>
+            <ul class="space-y-2">
+                <p class="font-medium text-[var(--text-primary)]">RESOURCES</p>
+                <li><NuxtLink to="#">Blog</NuxtLink></li>
+                <li><NuxtLink to="#">Cheat Sheet</NuxtLink></li>
+                <li><NuxtLink to="#">Channel Partners</NuxtLink></li>
+                <li><NuxtLink to="#">Affiliate Program</NuxtLink></li>
+            </ul>
+        </div>
+
+        <div>
+            <ul class="space-y-2">
+                <p class="font-medium text-[var(--text-primary)]">COMPARE</p>
+                <li><NuxtLink to="#">Session Recording</NuxtLink></li>
+                <li><NuxtLink to="#">Feature Flags</NuxtLink></li>
+                <li><NuxtLink to="#">Heatmaps</NuxtLink></li>
+                <li><NuxtLink to="#">Correlation Analysis</NuxtLink></li>
+            </ul>
+        </div>
+
+        <div>
+            <ul class="space-y-2">
+                <p class="font-medium text-[var(--text-primary)]">COMPANY</p>
+                <li><NuxtLink to="#">About Us</NuxtLink></li>
+                <li><NuxtLink to="#">Our Story</NuxtLink></li>
+                <li><NuxtLink to="#">Work With Us</NuxtLink></li>
+            </ul>
+        </div>
     </footer>
 </template>
 
@@ -8,6 +53,4 @@
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
